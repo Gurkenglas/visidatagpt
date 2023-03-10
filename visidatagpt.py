e6ace2ef7c17058fc2f7c685cafbd2b48da284ed
Gurkenglas <gurkenglas@hotmail.de>
2023-03-10 20:11:46 +0100

    lenses

--- visidatagpt.py
+++ visidatagpt.py
@@ -1,28 +1,40 @@
-# terminal spreadsheet with natural language processing
-
 import curses
-import csv
 import sys
+from lenses.ui import BoundLens, UnboundLens
+import lenses.optics as o
+import csv
+
+def boundlens(get, set):
+    return BoundLens(None, o.Lens(lambda none: get(), lambda none, new: set(new)))
+
+dividexbyten = UnboundLens(o.Lens(lambda yx: (yx[0], yx[1] // 10), lambda old, yx: (yx[0], yx[1] * 10)))
 
 def main(stdscr : curses.window):
-    with open(sys.argv[1]) as f:
-        reader = csv.reader(f)
-        for y, row in enumerate(list(reader)):
+    yx = boundlens(lambda: stdscr.getyx(),lambda yx: stdscr.move(*yx)) & dividexbyten
+    filename = sys.argv[1]
+    sheet = boundlens(lambda: list(csv.reader(open(filename))),lambda rows: csv.writer(open(filename, "w")).writerows(rows))
+    def render(what):
+        pos = yx.get()
+        stdscr.clear()
+        for y, row in enumerate(what.get()):
             for x, cell in enumerate(row):
-                stdscr.addstr(y, 10*x, cell)
-    stdscr.move(0, 0)
+                stdscr.addstr(y, x*10, cell)
+        yx.set(pos)
     commandlog = []
     while True:
-        row, col = stdscr.getyx()
-        data = stdscr.instr(row, col, 10)
-        move = lambda r,c: {"do": lambda: stdscr.move(row+r, col+c), "undo": lambda: stdscr.move(row, col)}
-        keymap ={"d": {"do": lambda: stdscr.addstr(row, col, " "*10), "undo": lambda: stdscr.addstr(row, col, data)}
-                ,"KEY_UP"  : move(-1, 0)
-                ,"KEY_DOWN": move(1, 0)
-                ,"KEY_RIGHT": move(0, 10)
-                ,"KEY_LEFT": move(0, -10)
-                ,"q": {"do": lambda: sys.exit(0), "undo": lambda: print("Can't undo that.")}
-                }
+        render(sheet)
+        y,x = yx.get()
+        cell = sheet[y][x]
+        value = cell.get()
+        keymap = {
+            "KEY_UP"   : {"desc": "go up"      , "do": lambda: yx[0]-1     , "undo": lambda: yx[0]+1},
+            "KEY_DOWN" : {"desc": "go down"    , "do": lambda: yx[0]+1     , "undo": lambda: yx[0]-1},
+            "KEY_RIGHT": {"desc": "go right"   , "do": lambda: yx[1]+1     , "undo": lambda: yx[1]-1},
+            "KEY_LEFT" : {"desc": "go left"    , "do": lambda: yx[1]-1     , "undo": lambda: yx[1]+1},
+            "d"        : {"desc": "flush cell" , "do": lambda: cell.set(""), "undo": lambda: cell.set(value)},
+            "e"        : {"desc": "edit"       , "do": lambda: cell.set(stdscr.getstr()), "undo": lambda: cell.set(value)},
+            "q"        : {"desc": "quit"       , "do": lambda: sys.exit(0)},
+        }
         cmd = keymap[stdscr.getkey()]
         commandlog.append(cmd)
         cmd["do"]()
