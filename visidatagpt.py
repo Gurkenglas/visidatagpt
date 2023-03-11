5c83649a70c0e178bbbb2a3dfce74ad4f1683b4a
Gurkenglas <gurkenglas@hotmail.de>
2023-03-11 21:54:07 +0100

    hard-fought repr on the plumbing

--- visidatagpt.py
+++ visidatagpt.py
@@ -1,39 +1,49 @@
-import curses, sys, varname
+import curses, sys, varname, json
 from lenses import lens, bind
-import regex as re
 
-relens = lambda self: lens.Lens(lambda s: self.search(s).group(0), lambda old, new: self.sub(new, old))
-
-optics = bind(open("optics.json").read()).Json().Each().F(lambda d: d.values()).Parts() #bootstrap
-def plumb(s, t):
-    for os, ot, o in optics.get():
-        if s == os:
-            if t == ot: return eval(o)
-            ott = plumb(ot, t)
-            if ott: return eval(o) & ott
+optics = bind(open("optics.json").read()).Json() #bootstrap
+def plumb(source, target):
+    for row in optics.get():
+        s, t, what, forward, backward = row["s"], row["t"], row["what"], row["forward"], row["backward"]
+        backwardargs = f"{t}" if what == "Iso" else f"{s},{t}"
+        parens = f"(lambda {s}:{forward},lambda {backwardargs}:{backward})"
+        optic = eval(f"lens.{what}{parens}")
+        class Plumb(optic.__class__):
+            def __repr__(self): return f"{what}{parens} from {s} to {t}"
+            def __init__(self):
+                self._optic = optic._optic
+                self.s = s
+                self.t = t
+        poptic = Plumb()
+        if source == s:
+            if t == target: return poptic
+            ott = plumb(t, target)
+            if ott: return poptic & ott
 
 def sheet():
-    return bind(f"{varname.varname()}.json") & plumb("file","lists")
+    return bind(f"{varname.varname()}.json") & plumb("file","dicts")
 optics = sheet()
-help = sheet()
+cmds = sheet()
 
 @curses.wrapper
 def main(window : curses.window):
-    rc = bind(window) & plumb("window", "rc")
-    sheets = [optics, help]
+    rc = bind(window) & plumb("window", "rcx")
+    sheets = [optics, cmds]
     commandlog = []
     while True:
         r,c = rc.get()
-        rows = sheets[-1].get()
-        for rindex, row in enumerate(rows):
+        view = sheets[-1] & plumb("dicts","lists")
+        colwidths = [max(len(str(cell)) for cell in col) for col in zip(*view.get())]
+        for rindex, row in enumerate(view.get()):
             for cindex, cell in enumerate(row):
-                window.addstr(*plumb("yx","rc").flip().get()((rindex,cindex)), str(cell))
+                window.addstr(*(bind((rindex,cindex)) & plumb("yx","rcx").flip()).get(), str(cell))
         rc.set((r,c))
-        cell = sheets[-1][r][c]
+        cell = view[r][c]
         value = cell.get()
         key = window.getkey()
-        for cmd in help.get():
-            if cmd[0] == key:
-                eval(cmd[2])
+        for cmd in cmds.get():
+            if cmd["key"] == key:
+                cmd["locals"] = locals()
+                eval(cmd["do"])
                 commandlog.append(cmd)
                 break
